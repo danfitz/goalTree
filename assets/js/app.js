56de50df25b4f8f2f9c6155e8a6ee85878ea9755
danielfitz <dan.fitz@pm.me>
2019-06-11 16:17:30 -0400

    Created basic methods for adding goals, renaming goals, and deleting goals

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,21 @@
 var goalTree = {
-  /* We need a way to store todos */
-  goals: {};
-  /* We need a way to add a todo */
+  /* We need a way to store goals */
+  goals: [],
+
   addGoal: function(goalText, depth) {
+    this.goals.splice(depth, 0, goalText);
+  },
 
+  renameGoal: function(goalText, depth) {
+    this.goals[depth] = goalText;
+  },
+
+  deleteGoal: function(depth) {
+    /* This deletes the target goal + all goals below it */
+    this.goals.splice(depth, this.goals.length-depth);
+  },
+
+  moveGoal: function(startingDepth, targetDepth) {
+    /* code */
   }
-
-  /* We need a way to add a todo under another todo */
 }
