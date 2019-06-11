8dbf94e2aecb47c275ac0e4aa6b31a28fbcf65a7
danielfitz <dan.fitz@pm.me>
2019-06-11 15:34:56 -0400

    Initializing HTML/CSS/JS

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+var goalTree = {
+  /* We need a way to store todos */
+  goals: {};
+  /* We need a way to add a todo */
+  addGoal: function(goalText, depth) {
+
+  }
+
+  /* We need a way to add a todo under another todo */
+}
